de29a5ce2069fd94c6651e1f99346cf0009b4242
kevin <357620917@qq.com>
2018-01-24 15:37:44 +0800

    support sms via aliyun

--- db/2018-01-03/01Structure.sql
+++ db/2018-01-03/01Structure.sql
@@ -15,3 +15,20 @@ CREATE TABLE SYUSRINF (
 ) COMMENT='用户基本信息表';
 
 
+# ************************************************************
+# Description: 验证码发送表.
+# User: kevin
+# Date: 2018-01-24
+# ************************************************************
+
+DROP TABLE IF EXISTS SYVRYMBL;
+CREATE TABLE SYVRYMBL (
+  SVMSEQCOD varchar(32) NOT NULL DEFAULT '' COMMENT '流水号',
+  SVMMOBILE varchar(32) DEFAULT '' COMMENT '手机号',
+  SVMVRYCOD varchar(32) DEFAULT '' COMMENT '验证码',
+  SVMUSEBFR varchar(32) DEFAULT '' COMMENT '已使用',
+  SVMEXPIRE timestamp NULL DEFAULT NULL COMMENT '过期时间',
+  PRIMARY KEY (SVMSEQCOD)
+) COMMENT='验证码发送表';
+
+
